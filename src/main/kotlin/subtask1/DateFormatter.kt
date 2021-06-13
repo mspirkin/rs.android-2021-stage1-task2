9566b78192846463f99adf3855c99092c066ebb6
Maksim Spirkin <dev@avtocontrol.com>
2021-06-14 01:16:44 +0300

    feat: Algoritm2

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -3,17 +3,18 @@ package subtask1
 import java.time.DateTimeException
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
         return try {
-            val date = LocalDate.of(year.toInt(),month.toInt(), day.toInt() )
-            date.format(DateTimeFormatter.ofPattern("dd MMMM, EEEE"))
+            val date = LocalDate.of(year.toInt(),month.toInt(), day.toInt())
+            date.format(DateTimeFormatter.ofPattern("dd MMMM, EEEE", Locale.getDefault()))
         } catch (ex:DateTimeException) {
             "Такого дня не существует"
         }
-        throw NotImplementedError("Not implemented")
+//        throw NotImplementedError("Not implemented")
     }
 }
